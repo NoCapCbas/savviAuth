66a248b2a5bf615a528e3e2e85095db429b01c7b
Damon <ddiazny919@gmail.com>
2024-09-25 05:57:25 -0400

    fixed login responsivness

--- web/src/components/pages/AuthenticationPage.tsx
+++ web/src/components/pages/AuthenticationPage.tsx
@@ -6,9 +6,9 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 export default function AuthenticationPage() {
   return (
     <>
-      <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="container h-[800px] flex-col items-center justify-center grid lg:px-0">
         <div className="lg:p-8">
-          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
               <Tabs className="w-full" defaultValue="login">
                 <TabsList className="w-full">
